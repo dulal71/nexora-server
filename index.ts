0d3ac7b37704676742576a60f531e1c5b5ff9e31
dulal71 <ahmeddulal4211@gmail.com>
2026-07-15 13:41:40 +0600

    add verifytoken

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient, Db, Collection, ObjectId } from "mongodb";
+import { MongoClient, Db, Collection, ObjectId, WithId, Document } from "mongodb";
 
 dotenv.config();
+declare global {
+  namespace Express {
+    interface Request {
+      user?: WithId<Document>;
+    }
+  }
+}
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -16,12 +23,16 @@ const dbName = process.env.DB_NAME as string;
 const client = new MongoClient(uri);
 let db: Db;
 let products: Collection;
+let userSession:Collection;
+let userCollection:Collection;
 
 async function connectDB(): Promise<void> {
   try {
     await client.connect();
     db = client.db(dbName);
     products= db.collection('products')
+    userSession=db.collection('session')
+    userCollection=db.collection('user')
     console.log("MongoDB Atlas connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
@@ -29,44 +40,55 @@ async function connectDB(): Promise<void> {
   }
 }
 
-//Get product By Id 
-app.get('/api/product/:id',async (req: Request, res: Response)=>{
-  try{
-    const productId =req.params.id as string
-    const query = {
-      _id:new ObjectId(productId)
-    }
-    const result = await products.findOne(query)
-     res.status(201).send(result);
-  }catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message || 'Failed to create product',
-    });
-  }
-})
 
-// Add New Product
-app.post('/api/products', async (req: Request, res: Response) => {
-  try {
-    const product = req.body;
-    const newProduct={
-      ...product,
-       createdAt: new Date()
-    }
-    
-    const result = await products.insertOne(newProduct);
-    res.status(201).send(result);
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message || 'Failed to create product',
-    });
-  }
-});
+// User Token Verify
 
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers?.authorization;
+ 
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(" ")[1];
+ 
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const query ={token:token}
+  const session = await userSession.findOne(query)
+  if (!session) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const userId=session.userId;
+  const userQuery={
+    _id:userId
+  }
+  const user = await userCollection.findOne(userQuery)
+  if(!user){
+     return res.status(401).send({ message: 'unauthorized access' });
+  }
+  req.user=user
+  next()
+};
+
+// verify-admin
+const verifyAdmin=async(req:Request,res:Response,next:NextFunction)=>{
+  if(req.user?.role !== 'admin'){
+    return res.status(403).send({message:'forbidden access'})
+  }
+next()
+}
+
+
+// verify-user
+const verifyUser=async(req:Request,res:Response,next:NextFunction)=>{
+  if(req.user?.role !== 'user'){
+    return res.status(403).send({message:'forbidden access'})
+  }
+next()
+}
 // Get Product
-app.get('/api/products', async (req: Request, res: Response) => {
+app.get('/api/products' ,async (req: Request, res: Response) => {
   try {
     const search = req.query.search as string
     const sort = req.query.sort as string 
@@ -126,10 +148,50 @@ if(category){
 
   }
 });
+//Get product By Id 
+app.get('/api/product/:id',verifyToken, async (req: Request, res: Response)=>{
+  try{
+    const productId =req.params.id as string
+    const query = {
+      _id:new ObjectId(productId)
+    }
+    const result = await products.findOne(query)
+     res.status(201).send(result);
+  }catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Failed to create product',
+    });
+  }
+})
+
+// Add My Product
+ app.get('/api/my-orders', verifyToken, verifyUser, async (req:Request, res:Response) => {  })
+
+ // Add New Product
+app.post('/api/products',verifyToken,verifyAdmin, async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    const newProduct={
+      ...product,
+       createdAt: new Date()
+    }
+    
+    const result = await products.insertOne(newProduct);
+    res.status(201).send(result);
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Failed to create product',
+    });
+  }
+});
+
+
 
 // Delete Product 
 
-app.delete('/api/product/:id',async(req:Request,res:Response)=>{
+app.delete('/api/product/:id',verifyToken,verifyAdmin,async(req:Request,res:Response)=>{
   try{
     const productId = req.params.id as string ;
     console.log('productId',productId);
@@ -151,7 +213,7 @@ const result = await products.deleteOne(query)
 })
 
 // Edit Product
-app.patch('/api/product/:id',async(req:Request,res:Response)=>{
+app.patch('/api/product/:id',verifyToken,verifyAdmin,async(req:Request,res:Response)=>{
   try{
     const productId = req.params.id as string ;
     const data = req.body 
